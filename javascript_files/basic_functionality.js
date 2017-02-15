2f613f6f54c37205a38fa3eee0aa7cf0d72a9a7d
gabrieldornianu <gabrieldornianu@yahoo.com>
2017-02-15 21:32:58 +0200

    Removed the old structure

--- javascript_files/basic_functionality.js
+++ javascript_files/basic_functionality.js
@@ -1,5 +1,9 @@
 console.log("script working!");
 
+window.scroll({
+	behavior: 'smooth'
+});
+
 function parallax(e){
 	var y_position = window.pageYOffset;
 	var page_background = document.getElementsByClassName("page_background")[0];
