f81e29ada7164dbdd82850045e1d191110f047bd
gabrieldornianu <gabrieldornianu@yahoo.com>
2017-02-16 20:04:16 +0200

    Added a script that changes background colors

--- javascript_files/basic_functionality.js
+++ javascript_files/basic_functionality.js
@@ -1,12 +1,15 @@
 console.log("script working!");
 
+var colors = ["#3498db ", "#85929e", "#d35400"];
+
 window.scroll({
 	behavior: 'smooth'
 });
 
-function parallax(e){
-	var y_position = window.pageYOffset;
-	var page_background = document.getElementsByClassName("page_background")[0];
-	page_background.style.top = y_position * .4 + "px";
-
+(function(){
+	var icons = document.getElementsByClassName('icons');
+	for(let i=0; i<icons.length; i++){
+		var number = Math.floor(Math.random()*3);
+		icons[i].style.backgroundColor = colors[number];
 	}
+})();
